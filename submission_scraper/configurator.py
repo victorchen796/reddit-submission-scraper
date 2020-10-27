a52732cca20f184a4d8e05c2a51db6d42cbada3a
victorchen796 <victorchen796@gmail.com>
2020-10-26 21:47:59 -0400

    Update formatting and rename edit_config.py

--- submission_scraper/configurator.py
+++ submission_scraper/configurator.py
@@ -7,6 +7,7 @@ required for PRAW.
 
 import json
 import os
+
 import logger
 
 script_path = os.path.abspath(__file__)
@@ -14,17 +15,18 @@ script_dir = os.path.split(script_path)[0]
 rel_path = "resources/config.json"
 config_path = os.path.join(script_dir, rel_path)
 
+
 def check_config():
     """
     Checks if config.json's values are null.
-    
+
     If at least one value is null, change all values to null.
     Returns True if no values are null and False otherwise.
     """
     with open(config_path, 'r') as f:
         configs = json.loads(f.read())
-    
-    if (configs['client_id'] is None or configs['client_secret'] is None or 
+
+    if (configs['client_id'] is None or configs['client_secret'] is None or
             configs['user_agent'] is None):
         logger.log("config.json is not initialized.")
         set_config(None, None, None)
@@ -33,11 +35,12 @@ def check_config():
         logger.log("config.json is initialized.")
         return True
 
+
 def set_config(id, secret, agent):
     """Sets values in config.json."""
     configs = {'client_id': id, 'client_secret': secret, 'user_agent': agent}
 
     with open(config_path, 'w') as f:
         f.write(json.dumps(configs, indent=2))
-    
+
     logger.log("Updated config.json: " + id + ", " + secret + ", " + agent)
