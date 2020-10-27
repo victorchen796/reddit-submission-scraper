cd7c24a01e418dfeec4a6d5840479dbb09dfdd51
victorchen796 <victorchen796@gmail.com>
2020-10-27 05:24:54 -0400

    Update formatting

--- submission_scraper/log.py
+++ submission_scraper/log.py
@@ -9,7 +9,7 @@ import os
 
 script_path = os.path.abspath(__file__)
 script_dir = os.path.split(script_path)[0]
-rel_path = "resources/log.txt"
+rel_path = 'resources/log.txt'
 log_path = os.path.join(script_dir, rel_path)
 
 
@@ -17,6 +17,7 @@ def log(message):
     """Adds a message to the end of the log"""
     time = datetime.now()
     time_string = time.strftime("%Y-%m-%d %H:%M:%S")
+
     with open(log_path, 'a') as f:
         f.write("[" + time_string + "] " + message + "\n")
 
