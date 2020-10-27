cb511e5df0208cc7bca0887ace7d5f5e6d4d6b27
victorchen796 <victorchen796@gmail.com>
2020-10-27 05:26:15 -0400

    Update formatting and log messages

--- submission_scraper/config.py
+++ submission_scraper/config.py
@@ -8,11 +8,11 @@ required for PRAW.
 import json
 import os
 
-import logger
+import log
 
 script_path = os.path.abspath(__file__)
 script_dir = os.path.split(script_path)[0]
-rel_path = "resources/config.json"
+rel_path = 'resources/config.json'
 config_path = os.path.join(script_dir, rel_path)
 
 
@@ -28,19 +28,41 @@ def check_config():
 
     if (configs['client_id'] is None or configs['client_secret'] is None or
             configs['user_agent'] is None):
-        logger.log("config.json is not initialized.")
         set_config(None, None, None)
         return False
     else:
-        logger.log("config.json is initialized.")
         return True
 
 
-def set_config(id, secret, agent):
+def set_config(client_id, client_secret, user_agent):
     """Sets values in config.json."""
-    configs = {'client_id': id, 'client_secret': secret, 'user_agent': agent}
+    configs = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'user_agent': user_agent
+    }
 
     with open(config_path, 'w') as f:
         f.write(json.dumps(configs, indent=2))
 
-    logger.log("Updated config.json: " + id + ", " + secret + ", " + agent)
+    log.log("Set client_id to \"" + client_id + "\".")
+    log.log("Set client_secret to \"" + client_secret + "\".")
+    log.log("Set user_agent + \"" + user_agent + "\".")
+
+
+def get_config():
+    """Returns the config values as a string"""
+    with open(config_path, 'r') as f:
+        configs = json.loads(f.read())
+
+    config_string = "client_id = " + configs['client_id'] + \
+        "\nclient_secret = " + configs['client_secret'] + \
+        "\nuser_agent = " + configs['user_agent'] + "\n"
+
+    log.log("Got config values")
+    return config_string
+
+
+def reset():
+    """Clears all fields of config"""
+    set_config(None, None, None)
