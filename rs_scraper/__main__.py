cb7cfc9da99864a54dae38d84a46cabc088ca9aa
victorchen796 <victorchen796@gmail.com>
2020-10-28 19:11:11 -0400

    Add __main__.py

---
+++ rs_scraper/__main__.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    major = sys.version_info[0]
+    minor = sys.version_info[1]
+
+    python_version = str(sys.version_info[0]) + "." + \
+        str(sys.version_info[1]) + "." + str(sys.version_info[2])
+
+    if major != 3 or minor < 6:
+        print("reddit-submission-scraper requires Python 3.6+.\nYou are " +
+              "using Python %s, which is not supported by " +
+              "reddit-submission-scraper." % (python_version))
+        sys.exit(1)
+
+    import rs_scraper.py
+    rs_scraper.main()
