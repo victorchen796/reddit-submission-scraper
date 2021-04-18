f3c3a83daecb46caffdf682a8a99fc2d4d24cc6d
victorchen796 <victorchen796@gmail.com>
2021-04-18 16:41:23 -0400

    Update config.py

--- rss/config.py
+++ rss/config.py
@@ -1,55 +1,27 @@
+from resources import get_config, update_config
+
+
 """
-edit_config module
-
-This module defines functions used to edit the configuration settings
-required for PRAW.
+config:
+{
+  'client_id': '<client_id>',
+  'client_secret': '<client_secret>',
+  'user_agent': '<user_agent>'
+}
 """
-
-import json
-import os
-
-script_path = os.path.abspath(__file__)
-script_dir = os.path.split(script_path)[0]
-rel_path = 'resources/config.json'
-config_path = os.path.join(script_dir, rel_path)
+config = get_config()
 
 
-def check_config():
-    """
-    Checks if config.json's values are null.
-
-    If at least one value is null, change all values to null.
-    Returns True if no values are null and False otherwise.
-    """
-    with open(config_path, 'r') as f:
-        configs = json.loads(f.read())
-
-    if (configs['client_id'] is None or configs['client_secret'] is None or
-            configs['user_agent'] is None):
-        set_config(None, None, None)
-        return False
-    else:
-        return True
+def view():
+    return config
 
 
-def set_config(client_id, client_secret, user_agent):
-    """Sets values in config.json."""
-    configs = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'user_agent': user_agent
-    }
-
-    with open(config_path, 'w') as f:
-        f.write(json.dumps(configs, indent=2))
+def edit(client_id, client_secret, user_agent):
+    config['client_id'] = client_id
+    config['client_secret'] = client_secret
+    config['user_agent'] = user_agent
+    update_config(config)
 
 
-def get_config():
-    """Returns the config values as a dict"""
-    with open(config_path, 'r') as f:
-        return json.loads(f.read())
-
-
-def reset():
-    """Clears all fields of config"""
-    set_config(None, None, None)
+def clear():
+    edit(None, None, None)
